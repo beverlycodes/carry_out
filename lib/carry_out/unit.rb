a7b3fd366a761af0fc0229be0e63b2dbbb3b09f1
Ryan Fields <ryan.fields@twoleftbeats.com>
2017-03-27 17:53:31 -0400

    Remove automatic creation of attr reader/writer for parameters.

--- lib/carry_out/unit.rb
+++ lib/carry_out/unit.rb
@@ -21,31 +21,11 @@ module CarryOut
     end
 
     def self.parameter(method_name, var = nil)
-      unless var.nil?
-        define_method(method_name.to_sym) do |*args|
-          instance_variable_set("@#{var}", args.length == 0 ? true : args.first)
-          self
-        end
-      end
+      var ||= method_name
 
-      var = (var || method_name).to_s
-      instance_var = "@#{var}"
-  
-      unless self.respond_to?(var)
-        define_method(var) do |*args|
-          if args.length > 0
-            instance_variable_set(instance_var, args.first)
-            return self
-          end
-
-          instance_variable_get(instance_var)
-        end
-      end
-
-      unless self.respond_to?("#{var}=")
-        define_method("#{var}=") do |value|
-          instance_variable_set(instance_var, value)
-        end
+      define_method(method_name.to_sym) do |*args|
+        instance_variable_set("@#{var}", args.length == 0 ? true : args.first)
+        self
       end
     end
   end
--- test/carry_out/unit_test.rb
+++ test/carry_out/unit_test.rb
@@ -6,61 +6,65 @@ class UnitTest < Minitest::Test
   class ParameterizedUnit < Unit
     parameter :with_test, :value
     appending_parameter :and_test, :value
+
+    def execute; @value; end
   end
 
-  def test_that_parameter_sets_attribute
+  def test_that_unit_has_parameter_method
     value = 'test'
     unit = ParameterizedUnit.new.with_test(value)
 
-    assert_equal value, unit.value
-  end
+    assert_respond_to unit, :with_test
 
-  def test_that_parameter_provides_attribute_writer
-    unit = ParameterizedUnit.new
-    assert unit.respond_to?('value=')
-    unit.value = 'test'
-    assert_equal 'test', unit.value
+    result = unit.execute
+    assert_equal value, result
   end
 
   def test_that_appending_parameter_converts_to_array
     value = 'test'
     value2 = 'test2'
-    unit = ParameterizedUnit.new
+    result = ParameterizedUnit.new
       .with_test(value)
       .and_test(value2)
+      .execute
 
-    assert_kind_of Array, unit.value
-    assert_includes unit.value, value
-    assert_includes unit.value, value2
+    assert_kind_of Array, result
+    assert_includes result, value
+    assert_includes result, value2
   end
 
   def test_that_appending_parameter_works_in_isolation
     value = 'test'
 
-    unit = ParameterizedUnit.new
+    result = ParameterizedUnit.new
       .and_test(value)
+      .execute
 
-    assert_kind_of Array, unit.value
-    assert unit.value.length == 1, "Expected length of #{unit.value.length} to be 1"
-    assert_includes unit.value, value
+    assert_kind_of Array, result
+    assert result.length == 1, "Expected length of #{result.length} to be 1"
+    assert_includes result, value
   end
 
   def test_that_explicit_nil_is_appended_to
     value = 'test'
 
-    unit = ParameterizedUnit.new
+    result = ParameterizedUnit.new
       .with_test(nil)
       .and_test(value)
+      .execute
 
-    assert_kind_of Array, unit.value
-    assert unit.value.length == 2, "Expected length of #{unit.value.length} to be 2"
-    assert_includes unit.value, nil
-    assert_includes unit.value, value
+    assert_kind_of Array, result
+    assert result.length == 2, "Expected length of #{result.length} to be 2"
+    assert_includes result, nil
+    assert_includes result, value
   end
 
   def test_that_boolean_parameter_can_chain
-    unit = ParameterizedUnit.new.with_test.and_test('test')
+    result = ParameterizedUnit.new
+      .with_test
+      .and_test('test')
+      .execute
 
-    assert_equal [ true, 'test' ], unit.value
+    assert_equal [ true, 'test' ], result
   end
 end
